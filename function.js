7c5891b2525feeb85fb1d2aab4e767928e2bd65d
Deepanshu yadav <deepanshu.yadav@sensationsolutions.in>
2026-02-06 13:24:17 +0530

    hoisting and tdz

---
+++ function.js
@@ -0,0 +1,40 @@
+//  definition
+// function sum(a = 0, b = 0) {
+//   let result = a + b;
+//   return result;
+// }
+// // calling
+// console.log(sum(4, 6));
+
+//  default parameter:
+// function greet(name = "user") {
+//   console.log(name);
+// }
+// greet();
+
+// function tabele(n) {
+//   for (let i = 1; i <= 10; i++) {
+//     console.log(`${n} X ${i} = ${n * i}`);
+//   }
+// }
+
+// tabele(97);
+
+// function tabele(n) {
+//   Array.from({ length: 10 }, (_, i) => {
+//     console.log(`${n} X ${i + 1}= ${n * (i + 1)}`);
+//   });
+// }
+// tabele(97);
+
+//  normal function:
+// console.log(square(7));
+// function square(n) {
+//   return n * n;
+// }
+
+// console.log(square(7));
+// const square = function (n) {
+//   return n * n;
+// };
+// console.log(square(7));
---
+++ hoisting.js
@@ -0,0 +1,52 @@
+// a;
+// console.log(a);
+
+// a = 5;
+// console.log(a);
+
+// console.log(a);
+// a = 5;
+
+//  var:
+// var a;
+// console.log(a);
+
+// var a = 5;
+// console.log(a);
+
+// console.log(a);
+// var a = 5;
+
+// let
+// let a;
+// console.log(a);
+
+// let a = 5;
+// console.log(a);
+
+// console.log(a);
+// let a = 5;
+
+//  functions:
+// normal function:
+
+// greet();
+// function greet() {
+//   const name = "deepanshu";
+//   console.log(`hello ${name} `);
+// }
+
+//  expressions:
+
+console.log(greet);
+
+var greet = function () {
+  const name = "deepanshu";
+  console.log(`hello ${name} `);
+};
+// console.log(greet());
+greet();
+
+//  bts:
+// var greet;
+// console.log(greet);
